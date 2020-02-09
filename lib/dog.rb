bd8570fd20f537508933ea0bf119e9d477090399
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-09 01:51:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,13 @@ class Dog
   
  def initialize (name, breed = "Mutt")
    @name = name 
-   @breed = breed = "Mutt"
+   @breed = breed
  end 
 
- def name = 
+ def name = (name, breed = "Mutt")
    @name = name 
    @breed = breed
+ end 
  def name
   @name
  end 
